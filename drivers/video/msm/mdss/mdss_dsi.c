0afaa82739672a38b24cc3198cfe3d8272a0be7a
Pranav Vashi <neobuddy89@gmail.com>
2017-04-10 01:40:33 +0530

    Add State Notifier Driver
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -24,6 +24,10 @@
 #include <linux/leds-qpnp-wled.h>
 #include <linux/clk.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #include "mdss.h"
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
@@ -1260,6 +1264,11 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
+
+		#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+		#endif
+
 		break;
 	case MDSS_EVENT_BLANK:
 		power_state = (int) (unsigned long) arg;
@@ -1271,7 +1280,13 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state &= ~CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
+
 		rc = mdss_dsi_off(pdata, power_state);
+
+		#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+		#endif
+
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
